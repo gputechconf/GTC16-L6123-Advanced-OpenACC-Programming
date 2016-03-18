abb270e3e8b09a88962f99272c38779f2b3d5c6c
Jeff Larkin <jlarkin@nvidia.com>
2016-03-18 15:03:05 +0000

    Fixed pipelining task4 solution to use only 2 streams and to read the number of blocks from program arguments.

--- pipelining/c99/task4/main_solution.cpp
+++ pipelining/c99/task4/main_solution.cpp
@@ -1,7 +1,9 @@
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
+#include <cstring>
 #include <omp.h>
+#include <openacc.h>
 #include "constants.h"
 
 using namespace std;
@@ -9,27 +11,33 @@ using namespace std;
 #pragma acc routine seq
 unsigned char mandelbrot(int Px, int Py);
 
-int main() {
+int main( int argc, char **argv ) {
   
   size_t bytes=WIDTH*HEIGHT*sizeof(unsigned int);
   unsigned char *image=(unsigned char*)malloc(bytes);
-  int num_blocks = 8, block_size = (HEIGHT/num_blocks)*WIDTH;
+  int num_blocks, block_size;
   FILE *fp=fopen("image.pgm","wb");
   fprintf(fp,"P5\n%s\n%d %d\n%d\n","#comment",WIDTH,HEIGHT,MAX_COLOR);
   double st = omp_get_wtime();
+
+  num_blocks = 8;
+  if ( argc > 1 ) num_blocks = atoi(argv[1]);
+  block_size = (HEIGHT/num_blocks)*WIDTH;
+
+  acc_init(acc_device_nvidia);
 
 #pragma acc data create(image[WIDTH*HEIGHT])
   for(int block = 0; block < num_blocks; block++ ) {
     int start = block * (HEIGHT/num_blocks),
         end   = start + (HEIGHT/num_blocks);
-#pragma acc update device(image[block*block_size:block_size]) async(block)
-#pragma acc parallel loop async(block)
+#pragma acc update device(image[block*block_size:block_size]) async(block%2)
+#pragma acc parallel loop async(block%2)
     for(int y=start;y<end;y++) {
       for(int x=0;x<WIDTH;x++) {
         image[y*WIDTH+x]=mandelbrot(x,y);
       }
     }
-#pragma acc update self(image[block*block_size:block_size]) async(block)
+#pragma acc update self(image[block*block_size:block_size]) async(block%2)
   }
 #pragma acc wait
   
